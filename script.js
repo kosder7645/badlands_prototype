7932aac66b0a59174e685005f3a181ba4b88c986
kosder7645 <kostrowka123@gmail.com>
2025-01-17 16:40:21 +0100

    first version of physics

--- script.js
+++ script.js
@@ -4,27 +4,33 @@ const carSize = { length: 40, width: 25 };
 let debugMode = true;
 
 const maxSpeed = [4, -2];
-const adhesion = 0.9;
-const engineFriction = 0.01;
+const adhesion = 0.1; // 0 - lód, 1 - pełna przyczepność
+const engineFriction = 0.02;
+const brakingForce = 0.1;
+const acceleration = 0.2;
+const turnSpeed = 3;
 
 class player {
     constructor(x, y, color, rotation) {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.rotaton = rotation;
+        this.rotation = rotation;
         this.points = [
             { x: 0, y: 0 },
             { x: 0, y: 0 },
             { x: 0, y: 0 },
             { x: 0, y: 0 }
         ];
-        this.force = 0;
+        this.speed = 0;
+        const cosA = Math.round(Math.cos((this.rotation * Math.PI) / 180) * 100) / 100;
+        const sinA = Math.round(Math.sin((this.rotation * Math.PI) / 180) * 100) / 100;     
+        this.frontVector = { x: sinA, y: -cosA };
         this.calculatePoints();
     }
     calculatePoints() {
-        const cosA = Math.round(Math.cos((this.rotaton * Math.PI) / 180) * 100) / 100;
-        const sinA = Math.round(Math.sin((this.rotaton * Math.PI) / 180) * 100) / 100;
+        const cosA = Math.round(Math.cos((this.rotation * Math.PI) / 180) * 100) / 100;
+        const sinA = Math.round(Math.sin((this.rotation * Math.PI) / 180) * 100) / 100;
 
         const w2 = carSize.width / 2;
         const h2 = carSize.length / 2;
@@ -41,47 +47,31 @@ class player {
             y: this.y + p.x * sinA + p.y * cosA
         }));
     }
-    move(amount) {
-        const cosA = Math.round(Math.cos((this.rotaton * Math.PI) / 180) * 100) / 100;
-        const sinA = Math.round(Math.sin((this.rotaton * Math.PI) / 180) * 100) / 100;
-
-        this.x = this.x + amount * sinA;
-        this.y = this.y - amount * cosA;
-
+    move() {
+        this.x += this.speed * this.frontVector.x;
+        this.y += this.speed * this.frontVector.y;
+        this.speed *= 1 - engineFriction; // naturalne zwalnianie
         this.calculatePoints();
     }
+    
+    accelerate() {
+        this.speed += acceleration;
+        if (this.speed > maxSpeed[0]) this.speed = maxSpeed[0];
+    }
+    
+    brake() {
+        this.speed -= brakingForce;
+        if (this.speed < maxSpeed[1]) this.speed = maxSpeed[1];
+    }
+    
     rotate(angle) {
-        if (this.force != 0) {
-            this.rotaton = (this.rotaton + angle) % 360;
-        }
-
-        this.calculatePoints();
-    }
-    addForce(addedForce) {
-        this.force = this.force + addedForce;
-        if (this.force > maxSpeed[0]) {
-            this.force = maxSpeed[0];
-        }
-        if (this.force < maxSpeed[1]) {
-            this.force = maxSpeed[1];
-        }
-    }
-    calculateMove() {
-        if (this.force > 0) {
-            if (this.force - engineFriction < 0) {
-                this.force = 0;
-            } else {
-                this.force = this.force - engineFriction;
-            }
-        }
-        if (this.force < 0) {
-            if (this.force + engineFriction > 0) {
-                this.force = 0;
-            } else {
-                this.force = this.force + engineFriction;
-            }
-        }
-        this.move(this.force);
+        if (Math.abs(this.speed) > 0.2) {
+            let turnEffect = (1 - adhesion) * (this.speed / maxSpeed[0]);
+            this.rotation += angle * (1 - turnEffect);
+        }           
+        const cosA = Math.round(Math.cos((this.rotation * Math.PI) / 180) * 100) / 100;
+        const sinA = Math.round(Math.sin((this.rotation * Math.PI) / 180) * 100) / 100;     
+        this.frontVector = { x: sinA, y: -cosA };
     }
 }
 
@@ -120,7 +110,7 @@ document.addEventListener('keyup', function (event) {
 
 function gameTick() {
     if (keysPressed['w']) {
-        playerArray[0].addForce(0.2);
+        playerArray[0].accelerate();
     }
     if (keysPressed['a']) {
         playerArray[0].rotate(-1);
@@ -129,14 +119,14 @@ function gameTick() {
         playerArray[0].rotate(1);
     }
     if (keysPressed['s']) {
-        playerArray[0].addForce(-0.1);
+        playerArray[0].brake();
     }
 
     generateCanvas();
     setTimeout(gameTick, 5);
 
     playerArray.forEach((player) => {
-        player.calculateMove();
+        player.move();
     });
 }
 
